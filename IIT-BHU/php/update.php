81fc144292436a01608ea5cea75d512c246e4f69
Mohit Gupta <mohit.gupta.cse11@iitbhu.ac.in>
2014-01-04 08:37:26 +0530

    corrected error in update.php

--- IIT-BHU/php/update.php
+++ IIT-BHU/php/update.php
@@ -1,22 +1,27 @@
 <?php
-	$username = "root";
-	$password = "";
-	$hostname = "localhost";
+	$username = "a9919284_parking";
+	$password = "erika1";
+	$hostname = "mysql6.000webhost.com";
 
 	$dbhandle = mysql_connect($hostname, $username, $password) 
   		or die("Unable to connect to MySQL");
 	echo "Connected to MySQL<br>";
 	
-	$selected = mysql_select_db("parking",$dbhandle) 
+	$selected = mysql_select_db("a9919284_parking",$dbhandle) 
   		or die("Could not select examples");
+        $total =  intval($_POST[total]);
+        $avail  = intval($_POST[avail]);
+        $latitude = doubleval($_POST[latitude]);
+        $longitude  = doubleval($_POST[longitude]);
 	
-	$sql = 'INSERT INTO names '.
-       		'(name,total,avail,latitude,longitude) '.
-       		'VALUES ('$_POST[name]','$_POST[total]','$_POST[avail]','$_POST[latitude]','$_POST[longitude]')';
+        
+        $sql="INSERT INTO names (name, total, avail, latitude, longitude)
+              VALUES
+             ('$_POST[name]','$total','$avail','$latitude','$longitude')";
 	
 	$retval = mysql_query($sql);
 	if(! $retval ){
 		die('Could not enter data: ' . mysql_error());
 	}
-	//echo "Entered data successfully\n";
+	echo "Entered data successfully\n";
 ?>
